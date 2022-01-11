9a4bfb6388c0d96918923a7e0f4ea4724da22818
19990-barthelemy <73411525+19990-barthelemy@users.noreply.github.com>
2022-01-11 11:03:34 +0100

    Add files via upload

---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+
+@extends ('layout.app')
+@section ('content')
+<div class="loader">
+        <span class="lettre">C</span>
+        <span class="lettre">H</span>
+        <span class="lettre">A</span>
+        <span class="lettre">R</span>
+        <span class="lettre">G</span>
+        <span class="lettre">E</span>
+        <span class="lettre">M</span>
+        <span class="lettre">E</span>
+        <span class="lettre">N</span>
+        <span class="lettre">T</span>
+    </div>
+    <div>
+        <h1>all right, Bienvenue! </h1>
+    </div>
+   <script src="{{asset('js/app.js')}}"></script> 
+   <script src="{{asset('js/monjs.js')}}"></script> 
+   <script src="{{asset('js/bootstrap.bundle.min')}}"></script> 
+@endsection

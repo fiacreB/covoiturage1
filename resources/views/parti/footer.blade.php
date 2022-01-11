36427d56374193e338f50355178534d55c647371
Tchinda Cyrille <84178238+Cyrille27@users.noreply.github.com>
2022-01-11 10:29:23 +0100

    Add files via upload

---
+++ resources/views/parti/footer.blade.php
@@ -0,0 +1,49 @@
+<div class="container">
+  <footer class="row row-cols-5 ">
+    <div class="col">
+      <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
+        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
+      </a>
+      <p class="text-muted">Covoiturage 2021 &copy </p>
+    </div>
+    <div class="col">
+      <h5 class="text-white">A propos </h5>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><b>. </b>Qui somme nous ?</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><b>. </b> Nous contacter</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><b>. </b>Connexion/Inscription</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><b>. </b>Je modifie mes données personnelles</a></li>
+      </ul>
+     
+    </div>
+
+    <div class="col">
+    <h5 class="text-white">Nos partenaire </h5>
+      <ul class="nav flex-column">
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><b>. </b>Université de Dschang</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><b>. </b>Departement d'informatique</a></li>
+         
+      </ul>
+    </div>
+
+    
+    <div class="col">
+      <h5>Section</h5>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+      </ul>
+    </div>
+    <div class="col">
+    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"/></svg></a></li>
+      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"/></svg></a></li>
+      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"/></svg></a></li>
+    </ul>
+    </div>
+  </footer>
+</div>
+
